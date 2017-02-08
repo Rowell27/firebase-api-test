c8d6bc5cbe6fbb49514b4d215f1e7ee5fda86863
wattamoves00 <kpanuncio123@yahoo.com>
2017-02-08 19:36:05 +0800

    work on firebase api test

--- src/async/async.component.ts
+++ src/async/async.component.ts
@@ -3,12 +3,12 @@ import { AsyncService } from './async.service';
 
 @Component({
     selector: 'async-page',
-    template: '<h1>Welcome, {{ user }}!</h1>'
+    template: '<h1>Welcome,{{ user }}!</h1>'
 })
 
 export class AsyncComponent {
 
-    user = "Krisha"
+    user = " ... "
     constructor( private asyncService: AsyncService, private ngZone: NgZone ) {
         this.getUser()
     }
@@ -21,7 +21,6 @@ export class AsyncComponent {
     getUser() {
         this.asyncService.getUsersData()
             .then( data => {
-                console.log( "This user: ", this.user );
                 this.user = data;
                 this.renderPage();
                 console.log( "This user: ", this.user );
